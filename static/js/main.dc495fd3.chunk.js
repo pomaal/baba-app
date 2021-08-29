9c238fa98ce8e0170beb5fc3b0dfb768a7fd0373
pomaal <fokinmax2009@mail.ru>
2021-08-29 20:08:31 +0700

    Updates

---
+++ static/js/main.dc495fd3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbaba=this.webpackJsonpbaba||[]).push([[0],{125:function(n,t,e){},126:function(n,t,e){"use strict";e.r(t);e(0),e(21);var a=e(16),o=e.n(a);e(24),e(28),e(19),e(9),e(123),e(5),e.p,e(125);o.a.send("VKWebAppInit",{}),o.a.send("VKWebAppGetAds",{ad_format:"reward"}).then((function(n){return console.log(n.result)})).catch((function(n){return console.log(n)}))}},[[126,1,2]]]);
+//# sourceMappingURL=main.dc495fd3.chunk.js.map
